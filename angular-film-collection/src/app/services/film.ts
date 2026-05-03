429cb0a0ae8b78585dfe012be7da26c6e94ec27b
amelur <ameluckin@gmail.com>
2026-05-03 18:12:05 +0300

    feat: add film model, mock data and FilmService with signals

--- angular-film-collection/src/app/services/film.ts
+++ angular-film-collection/src/app/services/film.ts
@@ -1,6 +1,38 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal, computed } from '@angular/core';
+import { Film } from '../models/film';
+import { films } from '../data/films';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Film {}
+export class FilmService {
+  private filmsSignal = signal<Film[]>(films);
+
+  search = signal('');
+
+  films = computed(() => this.filmsSignal());
+
+  favorites = computed(() =>
+    this.filmsSignal().filter(f => f.isFavorite)
+  );
+
+  filteredFilms = computed(() => {
+    const query = this.search().toLowerCase();
+
+    return this.filmsSignal().filter(f =>
+      f.title.toLowerCase().includes(query)
+    );
+  });
+
+  getFilmById(id: number): Film | undefined {
+    return this.filmsSignal().find(f => f.id === id);
+  }
+
+  toggleFavorite(id: number) {
+    this.filmsSignal.update(list =>
+      list.map(f =>
+        f.id === id ? { ...f, isFavorite: !f.isFavorite } : f
+      )
+    );
+  }
+}
